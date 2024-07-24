531125f42093ec0dcb04186e3ab5e6a6668a964b
Daniel <42255752+daniel-qa-exe@users.noreply.github.com>
2024-07-23 21:34:05 -0300

    Update 02_login_steps.py
    modificações no codigo referente ao time de espera de ações

--- features/steps/02_login_steps.py
+++ features/steps/02_login_steps.py
@@ -1,19 +1,21 @@
 from behave import given,when,then
 from selenium.webdriver.common.by import By
 import requests
-import json
 import time
+
 
 @given(u'ao usuário estar na página de login')
 def step_on_login(context):
     browser_title = context.browser.title
     assert "Joga Junto" in browser_title, "titulo nao encontrado"
-
+    time.sleep(2)
+    
 @when(u'o usuário preencher os campos com as credenciais corretas')
 def step_fill_login(context):
     time.sleep(3)
     context.browser.find_element(By.NAME, "email").send_keys(context.email)
     context.browser.find_element(By.NAME, "password").send_keys(context.password)
+    time.sleep(2)
 
 @when(u'clicar no botão "iniciar sessão"')
 def step_submit_login(context):
@@ -24,5 +26,5 @@ def step_submit_login(context):
 def step_login_sucess(context):
     response = requests.get("https://projetofinal.jogajuntoinstituto.org/")
     if response.status_code == 200 or response.status_code == 201:
-        with open('reposta_login.txt', 'w') as file:
-            file.write(f"O status da request foi {response.status_code}")   
+        with open('reposta_requests.txt', 'w') as file:
+            file.write(f"O status da request de login foi {response.status_code}")   
